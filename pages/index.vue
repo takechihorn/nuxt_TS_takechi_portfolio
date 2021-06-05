ccb8a255de3661f68a5ce96882192dafe8065b95
近藤岳瑠 <takechihorn@gmail.com>
2021-06-05 18:08:55 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>This is Test</h1>
+    <TestComponent :user="testUser" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import TestComponent from '~/components/TestComponent.vue'
+
+@Component({
+  components: {
+    TestComponent,
+  },
+})
+export default class IndexPage extends Vue {
+  myname = {
+    firstName: 'takeru',
+    lastName: 'kondo',
+  }
+
+  get testUser() {
+    return this.myname
+  }
+}
+</script>
---
+++ pages/test.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>{{ helloWorld }}</div>
+</template>
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+
+@Component // <- 1
+export default class Index extends Vue /* <- 2 */ {
+  get helloWorld(): string {
+    // <- 3
+    return 'Hello world!'
+  }
+}
+</script>
